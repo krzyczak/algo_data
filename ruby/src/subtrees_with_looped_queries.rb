0a81b06cf45de606a94940417439b2ea0b2ce190
krzyczak <krzyczak@users.noreply.github.com>
2022-04-28 16:21:29 +0100

    fixed subtrees

--- ruby/src/subtrees_with_looped_queries.rb
+++ ruby/src/subtrees_with_looped_queries.rb
@@ -1,3 +1,5 @@
+# The queires loop is OK but there is bug. Figure out the bug in a single query array and then bring this back
+
 require "pry"
 
 class NodeX
@@ -15,19 +17,19 @@ module Subtrees
 
     # return queries.map { |u, c| counts["#{u}#{c}"] } if node.nil?
 
-    # queries.each do |query|
-    #   # need to take care of multiple queries
-    # end
-    q_number, q_char = queries.first
-    query = "#{q_number}#{q_char}"
+    queries.each do |queryX|
+      # need to take care of multiple queries
+      # binding.pry
+      q_number, q_char = queryX
+      query = "#{q_number}#{q_char}"
 
-    if node.value == q_number
-      current_subtrees << query
+      if node.value == q_number
+        current_subtrees << query
+      end
     end
 
     current_subtrees.each do |cs|
-      if q_char == text[node.value - 1] # in Java it would be text.charAt(q_number) as strings are 1-based index
-        # puts "Adding +1 for cs: #{cs}, node.value: #{node.value}"
+      if cs.split("").last == text[node.value - 1] # in Java it would be text.charAt(q_number) as strings are 1-based index
         counts[cs] += 1
       end
     end
@@ -53,6 +55,7 @@ puts "Expected: [2], Actual: #{result}"
 
 
 # root = NodeX.new(1, [ NodeX.new(2, [NodeX.new(4), NodeX.new(5)]), NodeX.new(3, [NodeX.new(6)]), NodeX.new(7) ])
+
 root = NodeX.new(1);
 root.children << NodeX.new(2)
 root.children << NodeX.new(3)
@@ -63,12 +66,6 @@ root.children[0].children << NodeX.new(5)
 
 root.children[1].children << NodeX.new(6)
 
-result = Subtrees.call(root, [[1, "a"]], "abaacab")
-puts "Expected: [4], Actual: #{result}"
 
-result = Subtrees.call(root, [[2, "b"]], "abaacab")
-puts "Expected: [1], Actual: #{result}"
-
-result = Subtrees.call(root, [[3, "a"]], "abaacab")
-puts "Expected: [2], Actual: #{result}"
-
+result = Subtrees.call(root, [[1, "a"], [2, "b"], [3, "a"]], "abaacab")
+puts "Expected: [4, 1, 2], Actual: #{result}"
